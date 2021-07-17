527e81707ece476aaea84bb8489eb4236bdd3700
Joëlle <castelli.jo@gmail.com>
2021-07-17 16:35:20 +0200

    move all functions to SDK class

--- oauth-client/SDK.php
+++ oauth-client/SDK.php
@@ -29,7 +29,10 @@ class SDK
         $this->configFile = $configFile;
     }
 
-    public function getProviders(): array
+    /**
+     * @return array|mixed
+     */
+    public function getProviders(): mixed
     {
         return $this->providers;
     }
@@ -70,6 +73,61 @@ class SDK
                 . "&state=" . $this->getState() . "$redirect'>Se connecter avec ". $provider['name']."</a></div>";
         }
         echo $str;
+    }
+
+    function handleError()
+    {
+        ["state" => $state] = $_GET;
+        echo "{$state} : Request cancelled";
+    }
+
+    function handleSuccess()
+    {
+        ["state" => $state, "code" => $code] = $_GET;
+        if ($state !== STATE) {
+            throw new RuntimeException("{$state} : invalid state");
+        }
+        // https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
+        getUser([
+            'grant_type' => "authorization_code",
+            "code" => $code,
+        ]);
+    }
+
+    function handleFbSuccess()
+    {
+        ["state" => $state, "code" => $code] = $_GET;
+        if ($state !== STATE) {
+            throw new RuntimeException("{$state} : invalid state");
+        }
+        // https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
+        $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
+        $result = file_get_contents($url);
+        $resultDecoded = json_decode($result, true);
+        ["access_token"=> $token] = $resultDecoded;
+        $userUrl = "https://graph.facebook.com/me?fields=id,name,email";
+        $context = stream_context_create([
+            'http' => [
+                'header' => 'Authorization: Bearer ' . $token
+            ]
+        ]);
+        echo file_get_contents($userUrl, false, $context);
+    }
+
+    function getUser($params)
+    {
+        $url = "http://oauth-server:8081/token?client_id=" . CLIENT_ID . "&client_secret=" . CLIENT_SECRET . "&" . http_build_query($params);
+        $result = file_get_contents($url);
+        $result = json_decode($result, true);
+        $token = $result['access_token'];
+
+        $apiUrl = "http://oauth-server:8081/me";
+        $context = stream_context_create([
+            'http' => [
+                'header' => 'Authorization: Bearer ' . $token
+            ]
+        ]);
+        echo file_get_contents($apiUrl, false, $context);
     }
 
 }
