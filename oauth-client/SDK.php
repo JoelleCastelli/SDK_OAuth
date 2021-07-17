cbc82b089200cbd5e25372e29a38b7d5e4fbd2cb
Joëlle <castelli.jo@gmail.com>
2021-07-17 15:30:19 +0200

    read providers.json config file

--- oauth-client/SDK.php
+++ oauth-client/SDK.php
@@ -3,19 +3,39 @@
 
 class SDK
 {
-    private string $configFile = 'config.yml';
+    private string $configFile = 'providers.json';
     protected array $providers;
 
     public function __construct()
     {
-        $this->providers = yaml_parse_file($this->configFile);
+        $str = file_get_contents($this->configFile);
+        $this->providers = json_decode($str, true);
     }
 
-    public function getProviders(): mixed
+    /**
+     * @return string
+     */
+    public function getConfigFile(): string
+    {
+        return $this->configFile;
+    }
+
+    /**
+     * @param string $configFile
+     */
+    public function setConfigFile(string $configFile): void
+    {
+        $this->configFile = $configFile;
+    }
+
+    public function getProviders(): array
     {
         return $this->providers;
     }
 
+    /**
+     * @param array $providers
+     */
     public function setProviders(mixed $providers): void
     {
         $this->providers = $providers;
