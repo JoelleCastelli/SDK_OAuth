fd167614b02f48a225d578cd48b663ee53dea974
Joëlle <castelli.jo@gmail.com>
2021-07-18 19:50:24 +0200

    cleaner and lighter SDK file

--- oauth-client/SDK.php
+++ oauth-client/SDK.php
@@ -5,7 +5,6 @@ class SDK
 {
     private string $configFile = 'providers.json';
     protected array $providers;
-    protected string $state;
 
     public function __construct()
     {
@@ -13,52 +12,9 @@ class SDK
         $this->providers = json_decode($str, true);
     }
 
-    /**
-     * @return string
-     */
-    public function getConfigFile(): string
-    {
-        return $this->configFile;
-    }
-
-    /**
-     * @param string $configFile
-     */
-    public function setConfigFile(string $configFile): void
-    {
-        $this->configFile = $configFile;
-    }
-
-    /**
-     * @return array
-     */
     public function getProviders(): array
     {
         return $this->providers;
-    }
-
-    /**
-     * @param array $providers
-     */
-    public function setProviders(mixed $providers): void
-    {
-        $this->providers = $providers;
-    }
-
-    /**
-     * @return string
-     */
-    public function getState(): string
-    {
-        return $this->state;
-    }
-
-    /**
-     * @param string $state
-     */
-    public function setState(string $state): void
-    {
-        $this->state = $state;
     }
 
     function handleLogin()
@@ -105,39 +61,34 @@ class SDK
     }
 
     function getToken($providerName, $params) {
-        foreach ($this->getProviders() as $provider => $data) {
-            if($provider == $providerName) {
-                $url = $data['access_token_url'] . "?client_id=" .$data['id']
-                . "&client_secret=" . $data['secret']
-                . "&" . http_build_query($params);
+        $provider = $this->getProviders()[$providerName];
 
-                $result = file_get_contents($url);
-                if($this->isJson($result)) {
-                    $result = json_decode($result, true);
-                    $token = $result['access_token'];
-                } else {
-                    $string = explode("&", $result)[0];
-                    $token = explode("=", $string)[1];
-                }
+        $url = $provider['access_token_url'] . "?client_id=" .$provider['id']
+        . "&client_secret=" . $provider['secret']
+        . "&" . http_build_query($params);
 
-                return $token;
-            }
+        $result = file_get_contents($url);
+        if($this->isJson($result)) {
+            $result = json_decode($result, true);
+            $token = $result['access_token'];
+        } else {
+            $string = explode("&", $result)[0];
+            $token = explode("=", $string)[1];
         }
+
+        return $token;
     }
 
     function getUser($providerName, $token)
     {
-        foreach ($this->getProviders() as $provider => $data) {
-            if($provider == $providerName) {
-                $apiUrl = $data['me_url'];
-                $context = stream_context_create([
-                    'http' => [
-                        'header' => 'Authorization: Bearer ' . $token
-                    ]
-                ]);
-                echo file_get_contents($apiUrl, false, $context);
-            }
-        }
+        $provider = $this->getProviders()[$providerName];
+        $userUrl = $provider['me_url'];
+        $context = stream_context_create([
+            'http' => [
+                'header' => 'Authorization: Bearer ' . $token
+            ]
+        ]);
+        echo file_get_contents($userUrl, false, $context);
     }
 
     public function isJson($string): bool
